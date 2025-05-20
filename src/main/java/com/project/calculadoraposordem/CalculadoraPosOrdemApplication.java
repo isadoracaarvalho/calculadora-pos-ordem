1a8d1b793cf3ad14ec29cc5d872a47e5e0be526c
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-05-20 16:26:16 -0300

    Estrutura base do projeto

---
+++ src/main/java/com/project/calculadoraposordem/CalculadoraPosOrdemApplication.java
@@ -0,0 +1,13 @@
+package com.project.calculadoraposordem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraPosOrdemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraPosOrdemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/calculadoraposordem/CalculadoraPosOrdemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.calculadoraposordem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraPosOrdemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
